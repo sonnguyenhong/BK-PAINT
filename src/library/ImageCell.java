68be6a6d0d71db2441f7fe235967819b19a9fe14
HP <HP@DESKTOP-Q41B3P0>
2021-05-13 08:22:50 +0700

    add package library

---
+++ src/library/ImageCell.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author HP
+ */
+public class ImageCell extends ImageIcon {
+    
+}
